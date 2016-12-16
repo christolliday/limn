c535859309c94ef3ecbfeeff9e7242b3eeaa9814
Chris Tolliday <ctolliday@gmail.com>
2016-12-16 01:13:41 -0800

    store constraints in nodes, and create basic node tree

--- src/main.rs
+++ src/main.rs
@@ -62,66 +62,94 @@ struct Node<'a> {
     right: Variable,
     top: Variable,
     bottom: Variable,
-    drawable: &'a WidgetDrawable,
+    drawable: Option<&'a WidgetDrawable>,
+    constraints: Vec<Constraint>,
+    nodes: Vec<Node<'a>>,
 }
 
 impl<'a>  Node<'a>  {
-    fn new<W: WidgetDrawable>(drawable: &'a W) -> Self {
+    fn new(drawable: Option<&'a WidgetDrawable>) -> Self {
         Node {
             left: Variable::new(),
             right: Variable::new(),
             top: Variable::new(),
             bottom: Variable::new(),
             drawable: drawable,
+            constraints: Vec::new(),
+            nodes: Vec::new(),
         }
     }
     fn print(&self, solver: &mut Solver) {
-        println!("{:?} {:?} {:?} {:?}", 
+        println!("{:?} {:?} {:?} {:?}",
             solver.get_value(self.left),
             solver.get_value(self.top),
             solver.get_value(self.right),
             solver.get_value(self.bottom));
     }
     fn draw(&self, solver: &mut Solver, c: Context, g: &mut G2d) {
-        let bounds = [
-                solver.get_value(self.left),
-                solver.get_value(self.top),
-                solver.get_value(self.right) - solver.get_value(self.left),
-                solver.get_value(self.bottom) - solver.get_value(self.top),
-                ];
-        self.drawable.draw(bounds, c, g);
+        if let Some(drawable) = self.drawable {
+            let bounds = [
+                    solver.get_value(self.left),
+                    solver.get_value(self.top),
+                    solver.get_value(self.right) - solver.get_value(self.left),
+                    solver.get_value(self.bottom) - solver.get_value(self.top),
+                    ];
+            drawable.draw(bounds, c, g);
+        }
+    }
+    // layout
+    fn add_constraint(&mut self, constraint: Constraint) {
+        self.constraints.push(constraint);
+    }
+    fn add_constraints(&mut self, constraints: &[Constraint]) {
+        self.constraints.extend_from_slice(constraints);
+    }
+    fn width(&mut self, width: Scalar, strength: f64) {
+        self.constraints.push(self.right - self.left |EQ(strength)| width)
+    }
+    fn height(&mut self, height: Scalar, strength: f64) {
+        self.constraints.push(self.bottom - self.top |EQ(strength)| height)
+    }
+    fn add_node(&mut self, node: Node<'a>) {
+        self.nodes.push(node);
     }
 }
 
-struct Ui {
+struct Ui<'a> {
+    window: Node<'a>,
     constraints: Vec<Constraint>,
     pub solver: Solver,
     window_width: Variable,
     window_height: Variable,
 }
-impl Ui {
+impl<'a> Ui<'a> {
     fn new(window_dim: Dimensions) -> Self {
+        let window = Node::new(None);
         let window_width = Variable::new();
         let window_height = Variable::new();
         let mut constraints = Vec::new();
-        constraints.push(window_width |GE(REQUIRED)| 200.0);
-        constraints.push(window_height |GE(REQUIRED)| 100.0);
         let mut solver = Solver::new();
         solver.add_edit_variable(window_width, STRONG).unwrap();
         solver.add_edit_variable(window_height, STRONG).unwrap();
         solver.suggest_value(window_width, window_dim.width).unwrap();
         solver.suggest_value(window_height, window_dim.height).unwrap();
-        Ui { solver: solver, constraints: constraints, window_width: window_width, window_height: window_height }
+        Ui { window: window, solver: solver, constraints: constraints, window_width: window_width, window_height: window_height }
     }
     fn resize_window(&mut self, window_dims: [u32; 2]) {
         self.solver.suggest_value(self.window_width, window_dims[0] as f64).unwrap();
         self.solver.suggest_value(self.window_height, window_dims[1] as f64).unwrap();
     }
-    fn add_constraints(&mut self, new_constraints: &[Constraint]) {
-        self.constraints.extend_from_slice(new_constraints);
-    }
     fn init(&mut self) {
+        for ref mut node in &mut self.window.nodes {
+            let constraints = &mut node.constraints;
+            self.constraints.append(constraints);
+        }
         self.solver.add_constraints(&self.constraints);
+    }
+    fn draw(&mut self, c: Context, g: &mut G2d) {
+        for ref node in &self.window.nodes {
+            node.draw(&mut self.solver, c, g);
+        }
     }
 }
 
@@ -136,29 +164,35 @@ fn main() {
     // Create the event loop.
     let mut events = WindowEvents::new();
 
-    let rect = &RectDrawable::new([1.0, 0.0, 0.0]);
-    let box1 = Node::new(rect);
+    let rect = RectDrawable::new([1.0, 0.0, 0.0]);
+    let mut box1 = Node::new(Some(&rect));
     let circle = EllipseDrawable::new([1.0, 0.0, 1.0]);
-    let box2 = Node::new(&circle);
-    let circle2 = EllipseDrawable::new([1.0, 0.0, 1.0]);
+    let mut box2 = Node::new(Some(&circle));
+    //let circle2 = EllipseDrawable::new([1.0, 1.0, 1.0]);
+    //let mut box3 = Node::new(Some(&circle2));
 
     let ui = &mut Ui::new(window_dim);
-    let constraints = &[
-        box1.top |EQ(REQUIRED)| 0.0, // top align
+
+    let box1_constraints = [
+        box1.top |EQ(REQUIRED)| 0.0,
+        box1.left |EQ(REQUIRED)| 0.0,
+        box1.left |LE(REQUIRED)| box1.right];
+    box1.width(50.0, WEAK);
+    box1.height(100.0, WEAK);
+    box1.add_constraints(&box1_constraints);
+
+    let box2_constraints = [
         box2.bottom |EQ(REQUIRED)| ui.window_height, // bottom align
-        box1.left |EQ(REQUIRED)| 0.0, // left align
         box2.right |EQ(REQUIRED)| ui.window_width, // right align
         box2.left |GE(REQUIRED)| box1.right, // no overlap
-        // positive widths
-        box1.left |LE(REQUIRED)| box1.right,
-        box2.left |LE(REQUIRED)| box2.right,
-        // preferred widths:
-        box1.right - box1.left |EQ(WEAK)| 50.0,
-        box2.right - box2.left |EQ(WEAK)| 100.0,
-        // heights
-        box1.bottom - box1.top |EQ(WEAK)| 100.0,
-        box2.bottom - box2.top |EQ(WEAK)| 100.0];
-    ui.add_constraints(constraints);
+        box2.left |LE(REQUIRED)| box2.right];
+    box2.width(100.0, WEAK);
+    box2.height(100.0, WEAK);
+    box2.add_constraints(&box2_constraints);
+
+    ui.window.add_node(box1);
+    ui.window.add_node(box2);
+    //box1.add_node(box3);
     ui.init();
 
     // Poll events from the window.
@@ -170,8 +204,7 @@ fn main() {
 
         window.draw_2d(&event, |c, g| {
             clear([0.8, 0.8, 0.8, 1.0], g);
-            box1.draw(&mut ui.solver, c, g);
-            box2.draw(&mut ui.solver, c, g);
+            ui.draw(c, g);
         });
     }
 }
