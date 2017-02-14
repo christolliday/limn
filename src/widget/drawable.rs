8224d0676eecc5b78c3d876727b656b7e9b1c205
Chris Tolliday <ctolliday@gmail.com>
2017-02-14 01:26:30 -0800

    draw_fn refactor pt. 1

--- src/widget/drawable.rs
+++ src/widget/drawable.rs
@@ -20,9 +20,17 @@ pub struct DrawArgs<'a, 'b: 'a> {
     pub graphics: &'a mut G2d<'b>,
 }
 
+pub struct DrawArgs2<'a, 'b: 'a> {
+    pub bounds: Rectangle,
+    pub parent_bounds: Rectangle,
+    pub glyph_cache: &'a mut GlyphCache,
+    pub context: Context,
+    pub graphics: &'a mut G2d<'b>,
+}
+
 pub struct Drawable {
     state: Box<Any>,
-    pub draw_fn: fn(DrawArgs),
+    pub draw_fn: Box<Fn(DrawArgs)>,
     pub mouse_over_fn: Option<fn(Point, Rectangle) -> bool>,
     pub style: Option<DrawableStyle>,
     pub props: PropSet,
@@ -30,10 +38,31 @@ pub struct Drawable {
 }
 
 impl Drawable {
+    pub fn new2<T: Any>(state: T, draw_fn: fn(&T, DrawArgs2)) -> Drawable {
+        let draw_fn = move |args: DrawArgs| {
+            let DrawArgs { state, bounds, parent_bounds, glyph_cache, context, graphics, .. } = args;
+            let state: &T = state.downcast_ref().unwrap();
+            draw_fn(state, DrawArgs2 {
+                bounds: bounds,
+                parent_bounds: parent_bounds,
+                glyph_cache: glyph_cache,
+                context: context,
+                graphics: graphics,
+            });
+        };
+        Drawable {
+            state: Box::new(state),
+            draw_fn: Box::new(draw_fn),
+            mouse_over_fn: None,
+            style: None,
+            has_updated: false,
+            props: PropSet::new(),
+        }
+    }
     pub fn new<T: Any>(state: T, draw_fn: fn(DrawArgs)) -> Drawable {
         Drawable {
             state: Box::new(state),
-            draw_fn: draw_fn,
+            draw_fn: Box::new(draw_fn),
             mouse_over_fn: None,
             style: None,
             has_updated: false,
--- src/widgets/primitives.rs
+++ src/widgets/primitives.rs
@@ -3,7 +3,7 @@ use std::f64::consts::PI;
 use graphics;
 use graphics::types::Color;
 
-use widget::drawable::{Drawable, DrawableStyle, StyleArgs, DrawArgs};
+use widget::drawable::{Drawable, DrawableStyle, StyleArgs, DrawArgs, DrawArgs2};
 use widget::property::PropSet;
 use widget::style::{self, Value, StyleField};
 use util::{Scalar, Rectangle, Point};
@@ -12,7 +12,7 @@ use color::*;
 pub fn rect_drawable(style: Vec<RectStyleField>) -> Drawable {
     let mut state = RectDrawState::default();
     style::apply_style(&mut state, &style, &PropSet::new());
-    let mut drawable = Drawable::new(state, draw_rect);
+    let mut drawable = Drawable::new2(state, draw_rect);
     drawable.style = Some(DrawableStyle::new(style, apply_rect_style));
     drawable
 }
@@ -50,9 +50,8 @@ pub fn apply_rect_style(args: StyleArgs) {
     style::apply_style(state, style, &args.props);
 }
 
-pub fn draw_rect(args: DrawArgs) {
-    let DrawArgs { state, bounds, context, graphics, .. } = args;
-    let state: &RectDrawState = state.downcast_ref().unwrap();
+pub fn draw_rect(state: &RectDrawState, args: DrawArgs2) {
+    let DrawArgs2 { bounds, context, graphics, .. } = args;
     if let Some(radius) = state.corner_radius {
         let points_per_corner = 8;
         let angle_per_step = 2.0 * PI / (points_per_corner * 4) as Scalar;
