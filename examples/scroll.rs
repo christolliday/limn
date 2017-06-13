b814aeb2118551bd5ae7ce7869ef0982935513b4
Chris Tolliday <ctolliday@gmail.com>
2017-06-12 19:51:14 -0700

    use 3x3 grid in scroll example, init using closure

--- examples/scroll.rs
+++ examples/scroll.rs
@@ -9,7 +9,7 @@ mod util;
 use limn::widget::{WidgetBuilder, WidgetBuilderCore};
 use limn::widgets::scroll::ScrollBuilder;
 use limn::drawable::rect::{RectDrawable, RectStyleable};
-use limn::util::Size;
+use limn::util::{Size, Color};
 use limn::color::*;
 use limn::layout::constraint::*;
 
@@ -24,27 +24,27 @@ fn main() {
         match_layout(&root_widget).padding(50.0));
 
     let mut rect_container = WidgetBuilder::new_named("rect_container");
-    rect_container.grid(2);
+    rect_container.grid(3);
     layout!(rect_container: size(Size::new(400.0, 400.0)));
 
-    let mut rect_tl = WidgetBuilder::new_named("rect_tl");
-    rect_tl.set_drawable_with_style(RectDrawable::new(),
-        style!(RectStyleable::BackgroundColor: RED));
-    let mut rect_tr = WidgetBuilder::new_named("rect_tr");
-    rect_tr.set_drawable_with_style(RectDrawable::new(),
-        style!(RectStyleable::BackgroundColor: GREEN));
-    let mut rect_bl = WidgetBuilder::new_named("rect_bl");
-    rect_bl.set_drawable_with_style(RectDrawable::new(),
-        style!(RectStyleable::BackgroundColor: BLUE));
-    let mut rect_br = WidgetBuilder::new_named("rect_br");
-    rect_br.set_drawable_with_style(RectDrawable::new(),
-        style!(RectStyleable::BackgroundColor: FUSCHIA));
+    {
+        let mut add_rect = |color| {
+            let mut rect = WidgetBuilder::new_named(&format!("rect_{:?}", color));
+            rect.set_drawable_with_style(RectDrawable::new(),
+                style!(RectStyleable::BackgroundColor: color));
+            rect_container.add_child(rect);
+        };
+        add_rect(GREEN);
+        add_rect(BLUE);
+        add_rect(RED);
+        add_rect(YELLOW);
+        add_rect(CYAN);
+        add_rect(FUSCHIA);
+        add_rect(WHITE);
+        add_rect(GRAY);
+        add_rect(BLACK);
+    }
 
-    rect_container
-        .add_child(rect_tl)
-        .add_child(rect_tr)
-        .add_child(rect_bl)
-        .add_child(rect_br);
     scroll_widget.add_content(rect_container);
     root_widget.add_child(scroll_widget);
 
