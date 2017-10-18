47c964bebfed0389fef01499c45992f9b68dfba8
Felix Schütt <felix.schuett@maps4print.com>
2017-10-18 09:42:09 +0200

    Second try of refactoring /resources

--- src/resources/font.rs
+++ src/resources/font.rs
@@ -1,9 +1,9 @@
 //! Module for actions on a font. 
 //! The underlying type for a font is 
 use rusttype::{self, Font};
+use errors::Error as LimnError;
 
-
-fn load_font_data(name: &str) -> Result<Vec<u8>, ::std::io::Error> {
+fn load_font_data(name: &str) -> Result<Vec<u8>, LimnError> {
     use std::fs::File;
     use std::io::Read;
     let mut file = File::open(format!("assets/fonts/{}.ttf", name)).expect("Font missing");
@@ -12,7 +12,7 @@ fn load_font_data(name: &str) -> Result<Vec<u8>, ::std::io::Error> {
     Ok(data)
 }
 
-pub fn load_font(name: &str) -> Result<Font, ::std::io::Error> {
+pub fn load_font(name: &str) -> Result<Font, > {
     let data = try!(load_font_data(name));
     let collection = rusttype::FontCollection::from_bytes(data);
     Ok(collection.into_font().unwrap())
--- src/resources/resources.rs
+++ src/resources/resources.rs
@@ -64,19 +64,3 @@ impl Resources {
         &self.font_instances[&(name.to_owned(), size)]
     }
 }
-
-
-fn load_font_data(name: &str) -> Result<Vec<u8>, ::std::io::Error> {
-    use std::fs::File;
-    use std::io::Read;
-    let mut file = File::open(format!("assets/fonts/{}.ttf", name)).expect("Font missing");
-    let mut data = Vec::new();
-    try!(file.read_to_end(&mut data));
-    Ok(data)
-}
-
-pub fn load_font(name: &str) -> Result<Font, ::std::io::Error> {
-    let data = try!(load_font_data(name));
-    let collection = rusttype::FontCollection::from_bytes(data);
-    Ok(collection.into_font().unwrap())
-}
