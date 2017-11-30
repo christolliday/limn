b64274b8becf075e34723e671b5317f31b970ee3
Chris Tolliday <ctolliday@gmail.com>
2017-11-30 02:39:25 -0800

    Start refactoring texture loading to not use image api, image resources should use names that correspond to paths only

--- src/widgets/glcanvas.rs
+++ src/widgets/glcanvas.rs
@@ -6,8 +6,8 @@ pub struct GLCanvasBuilder;
 
 impl GLCanvasBuilder {
     /// Creates a new `GLCanvasBuilder`, returns it in form of a `WidgetBuilder`
-    pub fn new(name: &str, texture_id: u64) -> WidgetBuilder {
-        let image_draw_state = GLCanvasState::new(name, texture_id);
+    pub fn new(texture_id: u64) -> WidgetBuilder {
+        let image_draw_state = GLCanvasState::new(texture_id);
         let mut widget = WidgetBuilder::new("glcanvas");
         widget.set_draw_state(image_draw_state);
         widget
