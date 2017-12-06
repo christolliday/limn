afcaab9fc30a07b2eca29a5b46003a6202e5994d
Chris Tolliday <ctolliday@gmail.com>
2017-12-06 07:36:32 -0800

    Add alpha parameter to ellipse, make controllable via slider in circles example

--- src/draw/ellipse.rs
+++ src/draw/ellipse.rs
@@ -5,15 +5,31 @@ use widget::draw::Draw;
 use geometry::{Rect, RectExt, Point, Size};
 use color::*;
 
+use webrender::api::*;
+
 component_style!{pub struct EllipseState<name="ellipse", style=EllipseStyle> {
     background_color: Color = BLACK,
     border: Option<(f32, Color)> = None,
+    alpha: f32 = 1.0,
 }}
 
 impl Draw for EllipseState {
     fn draw(&mut self, bounds: Rect, _: Rect, renderer: &mut RenderBuilder) {
         // rounding is a hack to prevent bug in webrender that produces artifacts around the corners
         let bounds = bounds.round();
+
+        if self.alpha != 1.0 {
+            renderer.builder.push_stacking_context(
+                &PrimitiveInfo::new(Rect::zero()),
+                ScrollPolicy::Fixed,
+                None,
+                TransformStyle::Flat,
+                None,
+                MixBlendMode::Normal,
+                vec![FilterOp::Opacity(PropertyBinding::Value(self.alpha), self.alpha)],
+            );
+        }
+
         if let Some((width, color)) = self.border {
             let width = if width < 2.0 { 2.0 } else { width };
             push_ellipse(renderer, bounds, bounds, color);
@@ -21,6 +37,10 @@ impl Draw for EllipseState {
         } else {
             push_ellipse(renderer, bounds, bounds, self.background_color);
         };
+
+        if self.alpha != 1.0 {
+            renderer.builder.pop_stacking_context();
+        }
     }
     fn is_under_cursor(&self, bounds: Rect, cursor: Point) -> bool {
         let radius = Size::new(bounds.width() / 2.0, bounds.height() / 2.0);
